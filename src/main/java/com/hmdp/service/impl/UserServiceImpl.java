2d518a770e55d150f96549dc3551f4a313404015
xujinyang <1441739534@qq.com>
2026-01-12 15:20:21 +0800

    黑马点评发送验证码代码块

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    /**
+     * 发送验证码
+     *
+     * @param phone   电话号码
+     * @param session 存入的session
+     * @return
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机格式错误");
+        }
+        String code= RandomUtil.randomNumbers(6);
+        session.setAttribute("code",code);
+        log.debug("成功发送验证码为:{}",code);
+        return Result.ok();
+    }
+}
