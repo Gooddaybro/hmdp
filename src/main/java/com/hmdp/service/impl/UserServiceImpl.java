149b8d9b3cd6d5648c9935ecfb1b70d4faeb80eb
xujinyang <1441739534@qq.com>
2026-01-15 10:53:08 +0800

    登录功能模块

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -32,12 +35,52 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
      */
     @Override
     public Result sendCode(String phone, HttpSession session) {
-        if(RegexUtils.isPhoneInvalid(phone)){
+        if (RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("手机格式错误");
         }
-        String code= RandomUtil.randomNumbers(6);
-        session.setAttribute("code",code);
-        log.debug("成功发送验证码为:{}",code);
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code", code);
+        log.debug("成功发送验证码为:{}", code);
         return Result.ok();
+    }
+
+    /**
+     * 登录接口
+     *
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机格式错误");
+        }
+        Object code = session.getAttribute("code");
+        String code1 = loginForm.getCode();
+        if (code1 == null || !code.equals(code1)) {
+            return Result.fail("验证码错误");
+        }
+        User user = query().eq("phone", phone).one();
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    /**
+     * 创建用户信息
+     *
+     * @param phone
+     * @return
+     */
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(6));
+        save(user);
+        return user;
     }
 }
